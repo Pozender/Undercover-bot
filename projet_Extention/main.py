824822ec61a4c2a0ebda1618e6cbe8584624a5ff
Pozender <66801105+Pozender@users.noreply.github.com>
2021-01-30 21:33:30 +0100

    Update main.py

--- projet_Extention/main.py
+++ projet_Extention/main.py
@@ -5,7 +5,8 @@ bot = discord.Client()
 @bot.event
 def on_ready():
     print("prêt à fonctionner")
+  
 
-token = "ODA1MTUxNTgzMTk2NDEzOTY0.YBWt2A.zJOsKclt6ee4vMk2GxRmAH8hfLY"
+token = "TOKEN"
 
 bot.run(token)
